ce01e7632729cf4e550697cd6ee7cae1e4ffc875
sethripman <sethripman@gmail.com>
2019-10-03 09:20:49 -0700

    added test cases for all move combinations

--- test/rockpaperscissor.test.js
+++ test/rockpaperscissor.test.js
@@ -1,16 +1,122 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import { checkResult } from '../check-result.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('should return win if paper played against rock', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const playerMove = 'paper';
+    const compMove = 'rock';
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const result = checkResult(playerMove, compMove);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(result, 'win');
+});
+
+test('should return win if rock played against scissors', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'rock';
+    const compMove = 'scissors';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'win');
+});
+
+test('should return win if scissors played against paper', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'scissors';
+    const compMove = 'paper';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'win');
+});
+
+test('should return lose if paper played against scissors', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'paper';
+    const compMove = 'scissors';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'lose');
+});
+
+test('should return lose if scissors played against rock', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'scissors';
+    const compMove = 'rock';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'lose');
+});
+
+test('should return lose if rock played against paper', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'rock';
+    const compMove = 'paper';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'lose');
+});
+
+test('should return draw if paper played against paper', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'paper';
+    const compMove = 'paper';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'draw');
+});
+
+test('should return draw if rock played against rock', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'rock';
+    const compMove = 'rock';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'draw');
+});
+
+test('should return draw if scissors played against scissors', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const playerMove = 'scissors';
+    const compMove = 'scissors';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkResult(playerMove, compMove);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 'draw');
 });
